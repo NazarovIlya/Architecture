421a33c98d80e989c612ee6bfaf829efe3ec56eb
NazarovIlya <eliakohai@gmail.com>
2023-09-30 00:40:10 +0300

    abstract class complited

---
+++ HW/HW01/ModelChangerObserver/ModelChangerObserver/ModelElements/VectorAbstractBase.cs
@@ -0,0 +1,18 @@
+﻿using ModelChangerObserver.Stuff;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelChangerObserver.ModelElements
+{
+  internal abstract class VectorAbstractBase
+  {
+    public Point3D location;
+    public Angle3D angle;
+
+    public virtual void Rotate(Angle3D angle) { }
+    public virtual void Move(Point3D point) { }
+  }
+}
