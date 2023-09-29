110ad1543087f77a010e936b4ea33bcec6139832
NazarovIlya <eliakohai@gmail.com>
2023-09-29 19:48:58 +0300

    renamed model canger model interface

---
+++ HW/HW01/ModelChangerObserver/ModelChangerObserver/InMemoryModel/IModelChangeObserver.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelChangerObserver.InMemoryModel
+{
+  internal interface IModelChangerObserver
+  {
+    public void ApplyUpdateModel();
+  }
+}
