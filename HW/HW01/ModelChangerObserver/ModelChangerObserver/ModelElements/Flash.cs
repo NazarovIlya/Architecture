7f787d182aaf7cf444af62e4f08352c99b382a93
NazarovIlya <eliakohai@gmail.com>
2023-09-30 00:40:47 +0300

    flash class complited

--- HW/HW01/ModelChangerObserver/ModelChangerObserver/ModelElements/Flash.cs
+++ HW/HW01/ModelChangerObserver/ModelChangerObserver/ModelElements/Flash.cs
@@ -1,12 +1,17 @@
-﻿using System;
+﻿using ModelChangerObserver.Stuff;
+using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ModelChangerObserver.ModelElements
 {
-  internal class Flash
+  internal class Flash : VectorAbstractBase
   {
+    public Color color;
+    public double power;
+    public Flash() { }
   }
 }
