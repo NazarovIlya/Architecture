24dbeb928ecd7a2c9b02cc2d5e80218ecb441f12
NazarovIlya <eliakohai@gmail.com>
2023-09-30 22:59:47 +0300

    abstract factory added, task#2 complited

--- HW/HW02/AbstractFabricWithInterface/Program.cs
+++ HW/HW02/AbstractFabricWithInterface/Program.cs
@@ -1,15 +1,60 @@
 ﻿using AbstractFabricWithInterface;
+using AbstractFabricWithInterface.Factories;
 using AbstractFabricWithInterface.Products;
 
 
-RewardItem gold = new Gold();
-RewardItem silver = new Silver();
-RewardItem diamond = new Diamond();
-RewardItem sapphire = new Supphire();
-RewardItem platinum = new Platinum();
+RewardItem gold = new Gold(); // получение золота
+RewardItem silver = new Silver(); // получение серебра
+RewardItem diamond = new Diamond(); // получение алмазов
+RewardItem sapphire = new Supphire(); // получение сапфиров
+RewardItem platinum = new Platinum(); // получение платины
+RewardItem pearl = new Pearl(); // получение жемчуга
+RewardItem amber = new Amber(); // получение янтаря
 
+// у каждой награды вызываем метод поздрвления
 gold.Open();
 silver.Open();
 diamond.Open();
 sapphire.Open();
 platinum.Open();
+pearl.Open();
+amber.Open();
+
+// подводим черту и начинаем нормальную работу
+Console.WriteLine($"\n\n{string.Join("", Enumerable.Repeat("*=*", 30))}\n");
+
+// создаём список фабрик в упрощенном стиле
+List<ItemFactory> factories = new List<ItemFactory>()
+{
+  new AmberFactory(),
+  new AmberFactory(),
+  new GoldFactory(),
+  new GoldFactory(),
+  new GoldFactory(),
+  new GoldFactory(),
+  new GoldFactory(),
+  new PlatinumFactory(),
+  new PlatinumFactory(),
+  new PlatinumFactory(),
+  new PlatinumFactory(),
+  new PlatinumFactory(),
+  new PearlFactory(),
+  new SilverFactory(),
+  new SilverFactory(),
+  new SupphireFactory(),
+  new SupphireFactory(),
+  new DiamonFactory(),
+  new DiamonFactory(),
+  new DiamonFactory(),
+  new DiamonFactory(),
+  new DiamonFactory(),
+};
+
+Random random = new Random();
+
+// Выводим награды в случайном порядке
+foreach (var factory in factories)
+{
+  int index = random.Next(0, factories.Count());
+  factories[index].OpenReward();
+}
