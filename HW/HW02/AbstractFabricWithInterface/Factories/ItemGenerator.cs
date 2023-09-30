346d1aa4e4f23be25fb423311f4c7ad4ca633e61
NazarovIlya <eliakohai@gmail.com>
2023-09-30 22:17:16 +0300

    item generator class added

---
+++ HW/HW02/AbstractFabricWithInterface/Factories/ItemGenerator.cs
@@ -0,0 +1,20 @@
+﻿using AbstractFabricWithInterface.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractFabricWithInterface.Factories
+{
+  internal abstract class ItemGenerator
+  {
+    public abstract IGameItem CreateItem();
+
+    public void OpenReward()
+    {
+      CreateItem().Open();
+    }
+
+  }
+}
