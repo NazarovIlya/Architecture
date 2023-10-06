d1c2a892c155999186c475c9f9c5ecde205904a3
NazarovIlya <eliakohai@gmail.com>
2023-10-06 23:16:59 +0300

    SRP project added

---
+++ HW/HW03/SRP/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+
+namespace SRP
+{
+  public class Employee
+  {
+    public string Name { get; set; }
+    public DateOnly Date { get; set; }
+
+    public Employee(string name, int year, int month, int day)
+    {
+      Name = name;
+      Date = new DateOnly(year, month, day);
+    }
+
+    public override string? ToString()
+    {
+      return string.Format("Name: {0}, date: {1}", Name, Date);
+    }
+  }
+}
