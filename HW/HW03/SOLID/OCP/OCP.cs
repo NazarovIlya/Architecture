d8b426e799a73ec72db9f936f08d89948f204216
NazarovIlya <eliakohai@gmail.com>
2023-10-06 23:15:00 +0300

    updated, bugs fixedd

--- HW/HW03/SOLID/OCP/OCP.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace OCP
-{
-  public class OCP
-  {
-
-  }
-}
--- HW/HW03/SOLID/Program.cs
+++ HW/HW03/SOLID/Program.cs
@@ -1,7 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-using SRP;
+﻿using SRP;
 
-Console.WriteLine("Hello, World!");
-
-Employee employee = new Employee("Yana", "06/10/2023");
-Console.WriteLine(employee.ToString());
+Console.WriteLine("SOLID");
+Console.WriteLine($"\n{string.Join("", Enumerable.Repeat("=", 50))}\n");
+Employee employee =  new(name: "Yana", year: 2023, month: 10, day: 6);
+Console.WriteLine(employee);
